74da41ed0538cb86252953da617ac853997532f6
Nikolay <evrinoma@gmail.com>
2019-02-07 14:04:29 +0300

    nelmio api-doc Swagger

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -11,10 +11,11 @@ namespace App\Controller;
 
 use App\Core\Core;
 use App\Manager\ModuleManager;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
 
 class DisplayController extends AbstractController
 {
@@ -37,17 +38,33 @@ class DisplayController extends AbstractController
             'listModules' => $module->getListModules(),
         ];
     }
-//endregion Public
+
+
+
 
     /**
      * @Rest\Get("/users", name="users")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns the rewards of an user"
+     * )
+     * @SWG\Parameter(
+     *     name="order",
+     *     in="query",
+     *     type="string",
+     *     description="The field used to order rewards"
+     * )
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DisplayController.php',
-        ]);
+        return $this->json(
+            [
+                'message' => 'Welcome to your new controller!',
+                'path'    => 'src/Controller/DisplayController.php',
+            ]
+        );
     }
+//endregion Public
 
 }
