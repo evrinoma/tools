b6fde8abaa315f35ebb8f4a3471c743e7e9cfe00
nikolns <nikolns@ite-ng.ru>
2019-08-13 17:36:40 +0300

    vue mail domain table v3 and status stale's and refactor

--- assets/js/components/VueTables2/VueTables2.vue
+++ assets/js/components/VueTables2/VueTables2.vue
@@ -1,27 +1,42 @@
 <template>
     <div class="ui container">
-    <vuetable ref="vuetable"
-              api-url="https://vuetable.ratiw.net/api/users"
-              :fields="fields"
-              pagination-path=""
-              @vuetable:pagination-data="onPaginationData"
-    ></vuetable>
-        <vuetable-pagination ref="pagination"
-               @vuetable-pagination:change-page="onChangePage"
-        ></vuetable-pagination>
+        <div class="vuetable-pagination ui basic segment grid">
+            <vuetable-pagination-info ref="paginationInfoTop"
+            ></vuetable-pagination-info>
+            <vuetable-pagination ref="paginationTop"
+                                 @vuetable-pagination:change-page="onChangePage"
+            ></vuetable-pagination>
+        </div>
+        <vuetable ref="vuetable"
+                  api-url="https://vuetable.ratiw.net/api/users"
+                  :fields="fields"
+                  :per-page="5"
+                  pagination-path=""
+                  @vuetable:pagination-data="onPaginationData"
+        ></vuetable>
+        <div class="vuetable-pagination ui basic segment grid">
+            <vuetable-pagination-info ref="paginationInfo">
+            </vuetable-pagination-info>
+
+            <vuetable-pagination ref="pagination"
+                                 @vuetable-pagination:change-page="onChangePage"
+            ></vuetable-pagination>
+        </div>
     </div>
 </template>
 
 <script>
     import Vuetable from 'vuetable-2/src/components/Vuetable';
     import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
+    import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
 
     export default {
         components: {
             Vuetable,
-            VuetablePagination
+            VuetablePagination,
+            VuetablePaginationInfo
         },
-        data () {
+        data() {
             return {
                 fields: [
                     'name', 'email',
@@ -43,26 +58,34 @@
                     {
                         name: 'salary',
                         titleClass: 'center aligned',
-                        dataClass: 'right aligned'
+                        dataClass: 'right aligned',
+                        // visible: false
                     }
                 ]
             }
         },
         methods: {
-            allcap (value) {
+            allcap(value) {
                 return value.toUpperCase()
             },
-            genderLabel (value) {
+            genderLabel(value) {
                 return value === 'M'
                     ? '<span class="ui teal label"><i class="large man icon"></i>Male</span>'
                     : '<span class="ui pink label"><i class="large woman icon"></i>Female</span>'
             },
-            onPaginationData (paginationData) {
+            onPaginationData(paginationData) {
                 this.$refs.pagination.setPaginationData(paginationData)
             },
-            onChangePage (page) {
+            onChangePage(page) {
                 this.$refs.vuetable.changePage(page)
-            }
+            },
+            onPaginationData(paginationData) {
+                this.$refs.paginationTop.setPaginationData(paginationData);
+                this.$refs.paginationInfoTop.setPaginationData(paginationData);
+
+                this.$refs.pagination.setPaginationData(paginationData);
+                this.$refs.paginationInfo.setPaginationData(paginationData);
+            },
         }
     }
 </script>
