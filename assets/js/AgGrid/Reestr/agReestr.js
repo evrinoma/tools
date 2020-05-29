2bc491046fe722a0fd4e2507f21dbff528121bc6
nikolns <0631>
2020-05-29 22:37:54 +0300

    Project split

--- assets/js/AgGrid/Reestr/agReestr.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import agReestr from './AgReestr';
-
-/* eslint-disable no-new */
-new Vue({
-    el: '#agReestr',
-    template: '<agReestr/>',
-    components: { agReestr }
-});
