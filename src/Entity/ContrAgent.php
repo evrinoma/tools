9095b8858d6c61fbad41dc8ed130a437a1fb07af
evrinoma <evrinoma@gmail.com>
2021-06-09 16:39:10 +0300

    refactoring rest status

--- src/Entity/ContrAgent.php
+++ src/Entity/ContrAgent.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Entity\Common\CreateByUser;
 use App\Entity\Common\UpdateByUser;
 use Evrinoma\ContrAgentBundle\Entity\BaseContrAgent;
+use Evrinoma\ContrAgentBundle\Model\AbstractBaseContrAgent;
 use JMS\Serializer\Annotation\Type;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -15,7 +16,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="contragent")
  * @ORM\Entity
  */
-class ContrAgent extends BaseContrAgent
+class ContrAgent extends AbstractBaseContrAgent
 {
     use CreateByUser, UpdateByUser;
 }
